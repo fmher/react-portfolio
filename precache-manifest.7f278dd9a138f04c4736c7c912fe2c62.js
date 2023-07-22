d0af745becc38175530c34d5cff7d2b868f4a33e
fmher <feumatthewher2@gmail.com>
2023-07-21 17:30:24 -0700

    Updates

---
+++ precache-manifest.7f278dd9a138f04c4736c7c912fe2c62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "702e9698645fb44227437e10a1ab2f2c",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "2b351073337c2d838289",
+    "url": "/react-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "550832fc5292c028c397",
+    "url": "/react-portfolio/static/css/main.b6c8ee77.chunk.css"
+  },
+  {
+    "revision": "2b351073337c2d838289",
+    "url": "/react-portfolio/static/js/2.04d28be0.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-portfolio/static/js/2.04d28be0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "550832fc5292c028c397",
+    "url": "/react-portfolio/static/js/main.edc51cec.chunk.js"
+  },
+  {
+    "revision": "6f6086f91a7ae7353c00",
+    "url": "/react-portfolio/static/js/runtime-main.873154d3.js"
+  }
+]);
